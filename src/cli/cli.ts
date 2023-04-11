48e978072c2e449a1c39736be7c4e46af822c7e6
Jairus Tanaka <jairus.v.tanaka@outlook.com>
2023-04-10 17:33:40 -0700

    add tokenizer

---
+++ src/cli/cli.ts
@@ -0,0 +1,5 @@
+const version = "v0.1.0";
+const args = process.argv.slice(2);
+if (args.includes("-h") || !args.length) {
+    console.log("Usage: fass --assemblyscript -o build/ schemas/*.fass");
+}
--- src/parser/tokenizer.ts
+++ src/parser/tokenizer.ts
@@ -1,3 +1,65 @@
 export class Tokenizer {
-    
+    public pos: number = 0;
+    constructor(public text: string) {
+
+    }
+    getToken(): string {
+        let startPos = this.pos;
+        // Skip whitespace and newlines
+        while (this.text.charAt(startPos) == " " || this.text.charAt(startPos) == "\n") startPos++;
+        let endPos = startPos;
+        // Find next whitespace or EOF
+        while (endPos < this.text.length) {
+            if (this.text.charAt(endPos) == " " || this.text.charAt(endPos) == "\n") {
+                this.pos = endPos;
+                return this.text.slice(startPos, endPos);
+            } else {
+                endPos++;
+            }
+        }
+        this.pos = endPos;
+        return this.text.slice(startPos, endPos);
+    }
+    getNextTokens(amount: number): string[] {
+        const result = new Array<string>(amount);
+        while (amount--) result.push(this.getToken());
+        return result;
+    }
 }
+
+const text = `struct Vec3 {
+    quad: Quadrant
+    x: f32
+    y: f32
+    z: f32
+}
+
+enum Quadrant {
+    TL
+    TR
+    BL
+    BR
+}`
+
+const tk = new Tokenizer(text);
+
+console.log(tk.getToken());
+console.log(tk.getToken());
+console.log(tk.getToken());
+console.log(tk.getToken());
+console.log(tk.getToken());
+console.log(tk.getToken());
+console.log(tk.getToken());
+console.log(tk.getToken());
+console.log(tk.getToken());
+console.log(tk.getToken());
+console.log(tk.getToken());
+console.log(tk.getToken());
+console.log(tk.getToken());
+console.log(tk.getToken());
+console.log(tk.getToken());
+console.log(tk.getToken());
+console.log(tk.getToken());
+console.log(tk.getToken());
+console.log(tk.getToken());
+console.log(tk.getToken());
