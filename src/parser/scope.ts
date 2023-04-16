dcd0149b52b87bf9a3eb2665be6e4ac5dec311ae
Jairus Tanaka <jairus.v.tanaka@outlook.com>
2023-04-15 19:22:34 -0700

    initial scope implementation

---
+++ src/parser/scope.ts
@@ -0,0 +1,30 @@
+import { Statement } from "./nodes/Statement";
+
+export class Scope {
+    public elements: ScopeElement[];
+    constructor() { }
+    addElement(element: ScopeElement) {
+        this.elements.push(element);
+    }
+    getElement(index?: number, name?: string) {
+        if (index) {
+            return this.elements[index];
+        } else if (name) {
+            let elementPosition = -1;
+            const element = this.elements.find((value, pos) => {
+                if (value.name == name) {
+                    elementPosition = pos;
+                    return value;
+                };
+            });
+            if (elementPosition === -1) return undefined;
+            return this.elements[elementPosition];
+        }
+        return undefined;
+    }
+}
+
+export class ScopeElement {
+    public name!: string;
+    public node!: Statement;
+}
--- src/parser/source.ts
+++ src/parser/source.ts
@@ -1,11 +1,16 @@
 import { Statement } from "./nodes/Statement";
+import { Scope } from "./scope";
+import { Tokenizer } from "./tokenizer";
 
 export class Source {
     public name!: string;
     public text!: string;
-    public stmts!: Statement[];
+    public tokenizer!: Tokenizer;
+    public stmts: Statement[] = [];
+    public scope: Scope = new Scope();
     constructor(name: string, text: string) {
         this.name = name;
         this.text = text;
+        this.tokenizer = new Tokenizer(this.text);
     }
 }
