d59bedd6ce02210dfe94fc477ff5005b902c5f6b
Jairus Tanaka <jairus.v.tanaka@outlook.com>
2023-04-16 07:37:03 -0700

    remove regex. string manipulation instead

--- src/parser/tokenizer.ts
+++ src/parser/tokenizer.ts
@@ -32,31 +32,55 @@ export class Tokenizer {
     }
 
     private getNextToken(): { value: string, index: number } | undefined {
-        const tokenRegExp = /[\w{}]+|:/g;
-        const match = tokenRegExp.exec(this.text.slice(this.currentTokenIndex));
-        if (match) {
-            const value = match[0];
-            const index = match.index + this.currentTokenIndex;
-            this.currentTokenIndex = index + value.length;
+        const text = this.text.slice(this.currentTokenIndex);
+        const len = text.length;
+        let value = "";
+        let index = -1;
+        let i = 0;
 
-            return { value, index };
+        while (i < len) {
+            const c = text[i];
+            if (c === " " || c === "\n") {
+                i++;
+                continue;
+            } else if (c === ":") {
+                value = ":";
+                index = this.currentTokenIndex + i;
+                break;
+            } else {
+                let start = i;
+                while (i < len) {
+                    const cc = text[i];
+                    if (cc === " " || cc === "\n" || cc === ":") {
+                        break;
+                    }
+                    i++;
+                }
+                value = text.slice(start, i);
+                index = this.currentTokenIndex + start;
+                break;
+            }
         }
-        return undefined;
+
+        if (value === "") {
+            return undefined;
+        }
+
+        this.currentTokenIndex = index + value.length;
+        return { value, index };
     }
-}
-/*
-const tokenizer = new Tokenizer(`struct Vec3 {
-    quad: Quadrant
-    x: f32
-    y: f32
-    z: f32
+
 }
 
-enum Quadrant {
-    TL = 1
-    TR = 2
-    BL = 3
-    BR = 4
+const tokenizer = new Tokenizer(`include "./Vec3.fass"
+include "./Movement.fass"
+
+struct Player {
+    name: char[8]
+    id: i32
+    pos: Vec3
+    movement: Movement
+    data: u8[5]
 }`);
 
-console.log(tokenizer.getAllTokens());*/
+console.log(tokenizer.getAllTokens());
