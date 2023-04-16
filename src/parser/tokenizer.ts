534e00f9cb89fe73cec25513511ef71dedc8ae60
Jairus Tanaka <jairus.v.tanaka@outlook.com>
2023-04-15 20:36:49 -0700

    fix parser

--- src/parser/tokenizer.ts
+++ src/parser/tokenizer.ts
@@ -32,14 +32,31 @@ export class Tokenizer {
     }
 
     private getNextToken(): { value: string, index: number } | undefined {
-        const tokenRegExp = /[\w]+|:/g;
+        const tokenRegExp = /[\w{}]+|:/g;
         const match = tokenRegExp.exec(this.text.slice(this.currentTokenIndex));
         if (match) {
             const value = match[0];
             const index = match.index + this.currentTokenIndex;
             this.currentTokenIndex = index + value.length;
+
             return { value, index };
         }
         return undefined;
     }
 }
+/*
+const tokenizer = new Tokenizer(`struct Vec3 {
+    quad: Quadrant
+    x: f32
+    y: f32
+    z: f32
+}
+
+enum Quadrant {
+    TL = 1
+    TR = 2
+    BL = 3
+    BR = 4
+}`);
+
+console.log(tokenizer.getAllTokens());*/
