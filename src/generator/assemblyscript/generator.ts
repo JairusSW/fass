3519e323a74814b8cadb14ae8936f7aeeae58769
Jairus Tanaka <jairus.v.tanaka@outlook.com>
2023-04-21 08:04:04 -0700

    remove zig. generator

--- src/generator/assemblyscript/generator.ts
+++ src/generator/assemblyscript/generator.ts
@@ -2,8 +2,44 @@ import { EnumDeclaration, StructDeclaration } from "../../parser/nodes";
 import { Parser } from "../../parser/parser";
 import { Source } from "../../parser/source";
 
+enum Offsets {
+    u8 = 1,
+    i8 = 1,
+    bool = 1,
+    u16 = 2,
+    i16 = 2,
+    u32 = 4,
+    i32 = 4,
+    f32 = 4,
+    u64 = 8,
+    i64 = 8,
+    f64 = 8
+}
+/**
+ * FASS Code Generator for the AssemblyScript Language
+ * 
+ * Stages:
+ * - Codegen
+ * - Sorting
+ * - Optimization
+ * - Output
+ * 
+ * Codegen:
+ * A bunch of load and store ops that push and pull data from a said buffer
+ * 
+ * Sorting:
+ * Perhaps we can have dynamic-length sequences at the end and static structs aligned to the start,
+ * so that there is no need to load a length-prefixed length of the sequence and add that to the offset.
+ * 
+ * Optimization:
+ * Many of these operations can be combined into a single 64-bit load/store or a SIMD v128 load/store.
+ * Take advantage of that to decrease overhead.
+ * 
+ * Output:
+ * Should be formatted correctly and be conherent.
+ */
 export class Generator {
-    public text!: string;
+    public text: string = "";
     public source!: Source;
     constructor(source: Source) {
         this.source = source;
@@ -20,32 +56,85 @@ export class Generator {
     }
     generateStruct(decl: StructDeclaration): string {
         let txt = `export class ${decl.name.value} {`;
-        let serialize = `@inline __FASS_SERIALIZE(output: ArrayBuffer, input: ${decl.name.value}): void {`;
-        let deserialize = `@inline __FASS_DESERIALIZE(input: ArrayBuffer, output: ${decl.name.value}): void {`;
+        let serialize = [`@inline __FASS_SERIALIZE(output: ArrayBuffer, input: ${decl.name.value}): void {`];
+        let deserialize = [`@inline __FASS_DESERIALIZE(input: ArrayBuffer, output: ${decl.name.value}): void {`];
         // DETECT STATIC STRUCT
         let size = `public __FASS_SIZE(): u32 = `;
 
         let offset = 0;
 
         for (const member of decl.members) {
-            // DYNAMICALLY DETECT ENUM
-            if (member.type.text == "Quadrant") {
-                serialize += `\n\tstore<u8>(changetype<usize>(output), input.${member.name.value});`;
-                deserialize += `\n\toutput.${member.name.value} = load<u8>(changetype<usize>(input));`;
-                offset += 1;
-            } else if (member.type.text === "f32") {
-                serialize += `\n\tstore<f32>(changetype<usize>(output), input.${member.name.value}, ${offset})`;
-                deserialize += `\n\toutput.${member.name.value} = load<f32>(changetype<usize>(input), ${offset});`;
-                offset += 4;
+            const namedType = member.type.text;
+            const declaredName = member.name.value;
+            const scopeElement = this.source.scope.getElement(undefined, member.type.text);
+            // If not in scope, must be primitive.
+            if (!scopeElement) {
+                if (member.type.text === "bool") {
+                    serialize.push(`\n\tstore<bool>(changetype<usize>(output), input.${declaredName}, ${offset});`);
+                    deserialize.push(`\n\toutput.${declaredName} = load<bool>(changetype<usize>(input), ${offset});`);
+                    offset += 1;
+                } else if (member.type.text === "u8") {
+                    serialize.push(`\n\tstore<u8>(changetype<usize>(output), input.${declaredName}, ${offset});`);
+                    deserialize.push(`\n\toutput.${declaredName} = load<u8>(changetype<usize>(input), ${offset});`);
+                    offset += 1;
+                } else if (member.type.text === "i8") {
+                    serialize.push(`\n\tstore<i8>(changetype<usize>(output), input.${declaredName}, ${offset});`);
+                    deserialize.push(`\n\toutput.${declaredName} = load<i8>(changetype<usize>(input), ${offset});`);
+                    offset += 1;
+                } else if (member.type.text === "u16") {
+                    serialize.push(`\n\tstore<u16>(changetype<usize>(output), input.${declaredName}, ${offset});`);
+                    deserialize.push(`\n\toutput.${declaredName} = load<u16>(changetype<usize>(input), ${offset});`);
+                    offset += 2;
+                } else if (member.type.text === "i16") {
+                    serialize.push(`\n\tstore<i16>(changetype<usize>(output), input.${declaredName}, ${offset});`);
+                    deserialize.push(`\n\toutput.${declaredName} = load<i16>(changetype<usize>(input), ${offset});`);
+                    offset += 2;
+                } else if (member.type.text === "u32") {
+                    serialize.push(`\n\tstore<u32>(changetype<usize>(output), input.${declaredName}, ${offset});`);
+                    deserialize.push(`\n\toutput.${declaredName} = load<u32>(changetype<usize>(input), ${offset});`);
+                    offset += 4;
+                } else if (member.type.text === "i32") {
+                    serialize.push(`\n\tstore<i32>(changetype<usize>(output), input.${declaredName}, ${offset});`);
+                    deserialize.push(`\n\toutput.${declaredName} = load<i32>(changetype<usize>(input), ${offset});`);
+                    offset += 4;
+                } else if (member.type.text === "u64") {
+                    serialize.push(`\n\tstore<u64>(changetype<usize>(output), input.${declaredName}, ${offset});`);
+                    deserialize.push(`\n\toutput.${declaredName} = load<u64>(changetype<usize>(input), ${offset});`);
+                    offset += 8;
+                } else if (member.type.text === "i64") {
+                    serialize.push(`\n\tstore<i64>(changetype<usize>(output), input.${declaredName}, ${offset});`);
+                    deserialize.push(`\n\toutput.${declaredName} = load<i64>(changetype<usize>(input), ${offset});`);
+                    offset += 8;
+                }
+                // FLOAT                
+                else if (member.type.text === "f32") {
+                    serialize.push(`\n\tstore<f32>(changetype<usize>(output), input.${declaredName}, ${offset});`);
+                    deserialize.push(`\n\toutput.${declaredName} = load<f32>(changetype<usize>(input), ${offset});`);
+                    offset += 4;
+                } else if (member.type.text === "f64") {
+                    serialize.push(`\n\tstore<f64>(changetype<usize>(output), input.${declaredName}, ${offset});`);
+                    deserialize.push(`\n\toutput.${declaredName} = load<f64>(changetype<usize>(input), ${offset});`);
+                    offset += 8;
+                }
+            } else {
+                // Here we have actual structures
+                if (scopeElement.node instanceof EnumDeclaration && scopeElement.name == namedType) {
+                    serialize.push(`\n\tstore<u8>(changetype<usize>(output), input.${declaredName});`);
+                    deserialize.push(`\n\toutput.${declaredName} = load<u8>(changetype<usize>(input));`);
+                    offset += 1;
+                }
             }
+
+            // Optimization stage
+
         }
         
         size += offset + ";";
-        serialize += "\n}";
-        deserialize += "\n}";
+        serialize.push("\n}");
+        deserialize.push("\n}");
 
-        txt += "\n" + serialize;
-        txt += "\n" + deserialize;
+        txt += "\n" + serialize.join("");
+        txt += "\n" + deserialize.join("");
 
         return txt;
     }
