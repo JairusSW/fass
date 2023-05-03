d78392af78ab5562d9b69a72d917e3c29f3326ce
Jairus Tanaka <jairus.v.tanaka@outlook.com>
2023-05-02 21:46:25 -0700

    remove debug in tokenizer.ts

--- src/parser/tokenizer.ts
+++ src/parser/tokenizer.ts
@@ -64,19 +64,3 @@ export class Tokenizer {
         return null;
     }
 }
-
-const tokenizer = new Tokenizer(`struct Vec3 {
-    // This is a comment
-    quad: Quadrant
-    x: f32
-    y: f32
-    z: f32
-}
-enum Quadrant {
-    TL = 1
-    TR = 2
-    BL = 3
-    BR = 4
-}`);
-
-console.log(tokenizer.getAllTokens())
